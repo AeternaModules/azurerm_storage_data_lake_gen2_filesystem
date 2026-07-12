5b8c3d612d727f6a26cab58c4625a51d4fee0072
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "storage_data_lake_gen2_filesystems" {
-  description = "All storage_data_lake_gen2_filesystem resources"
-  value       = azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems
-}
 output "storage_data_lake_gen2_filesystems_ace" {
-  description = "List of ace values across all storage_data_lake_gen2_filesystems"
-  value       = [for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : v.ace]
+  description = "Map of ace values across all storage_data_lake_gen2_filesystems, keyed the same as var.storage_data_lake_gen2_filesystems"
+  value       = { for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : k => v.ace }
 }
 output "storage_data_lake_gen2_filesystems_default_encryption_scope" {
-  description = "List of default_encryption_scope values across all storage_data_lake_gen2_filesystems"
-  value       = [for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : v.default_encryption_scope]
+  description = "Map of default_encryption_scope values across all storage_data_lake_gen2_filesystems, keyed the same as var.storage_data_lake_gen2_filesystems"
+  value       = { for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : k => v.default_encryption_scope }
 }
 output "storage_data_lake_gen2_filesystems_group" {
-  description = "List of group values across all storage_data_lake_gen2_filesystems"
-  value       = [for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : v.group]
+  description = "Map of group values across all storage_data_lake_gen2_filesystems, keyed the same as var.storage_data_lake_gen2_filesystems"
+  value       = { for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : k => v.group }
 }
 output "storage_data_lake_gen2_filesystems_name" {
-  description = "List of name values across all storage_data_lake_gen2_filesystems"
-  value       = [for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : v.name]
+  description = "Map of name values across all storage_data_lake_gen2_filesystems, keyed the same as var.storage_data_lake_gen2_filesystems"
+  value       = { for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : k => v.name }
 }
 output "storage_data_lake_gen2_filesystems_owner" {
-  description = "List of owner values across all storage_data_lake_gen2_filesystems"
-  value       = [for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : v.owner]
+  description = "Map of owner values across all storage_data_lake_gen2_filesystems, keyed the same as var.storage_data_lake_gen2_filesystems"
+  value       = { for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : k => v.owner }
 }
 output "storage_data_lake_gen2_filesystems_properties" {
-  description = "List of properties values across all storage_data_lake_gen2_filesystems"
-  value       = [for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : v.properties]
+  description = "Map of properties values across all storage_data_lake_gen2_filesystems, keyed the same as var.storage_data_lake_gen2_filesystems"
+  value       = { for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : k => v.properties }
 }
 output "storage_data_lake_gen2_filesystems_storage_account_id" {
-  description = "List of storage_account_id values across all storage_data_lake_gen2_filesystems"
-  value       = [for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : v.storage_account_id]
+  description = "Map of storage_account_id values across all storage_data_lake_gen2_filesystems, keyed the same as var.storage_data_lake_gen2_filesystems"
+  value       = { for k, v in azurerm_storage_data_lake_gen2_filesystem.storage_data_lake_gen2_filesystems : k => v.storage_account_id }
 }
 
